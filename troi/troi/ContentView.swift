c0b98087394913eb9548c24d7deec466b06baab9
Bayan999 <128540841+Bayan999@users.noreply.github.com>
2023-10-17 11:12:29 +0300

    Add files via upload

---
+++ troi/troi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  troi
+//
+//  Created by Bayan on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ troi/troi/troiApp.swift
@@ -0,0 +1,17 @@
+//
+//  troiApp.swift
+//  troi
+//
+//  Created by Bayan on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct troiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
